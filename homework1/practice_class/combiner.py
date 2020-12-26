377b7f14f1e0052f98ab6322f118525d05d108ed
Charvadar <vahagn.charvadaryan@gmail.com>
2020-12-26 12:31:06 +0400

    homework

--- homework1/practice_class/combiner.py
+++ homework1/practice_class/combiner.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import os
 
-def combineAllTxt:
-    with open(Path("AllCombined.txt"), "w") as f:
-        for root, directories, files in os.walk('.txt'):
-            for name in files:
-                with open(os.path.join(name), 'r') as rf:
-                    f.write(rf.read())
+def combin():
+	with open(Path("AllCombined.txt"), "w") as f:
+		directory = os.getcwd()
+		for filename in os.listdir(directory):
+			if filename.endswith(".txt"):
+				with open(Path(filename), 'r') as rf:
+					f.write(rf.read())
+		
